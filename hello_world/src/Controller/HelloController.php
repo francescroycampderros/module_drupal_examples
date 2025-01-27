567a077df922a9a30fd68f29bc9ea8504a4ebbb7
francescroycampderros <francescroy@francescroy.com>
2025-01-27 18:30:09 +0100

    Moving forward

--- hello_world/src/Controller/HelloController.php
+++ hello_world/src/Controller/HelloController.php
@@ -4,11 +4,34 @@
  * Contains \Drupal\hello_world\Controller\HelloController.
  */
 namespace Drupal\hello_world\Controller;
-class HelloController {
+
+use Drupal\Core\Render\HtmlResponse;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Controller\ControllerBase;
+
+class HelloController extends ControllerBase {
+
+
+  protected $dbConnection;
+
+
+  public function __construct(Connection $connectionService){
+    $this->dbConnection = $connectionService;
+  }
+
   public function content() {
-    return array(
-      '#type' => 'markup',
-      '#markup' => t('Hello, World!'),
-    );
+    
+    // Si vols que es vegi amb el mateix tema:
+    #return array(
+    #  '#type' => 'markup',
+    #  '#markup' => ('Hello, World!'),
+    #);
+  
+    // Si no:
+    $query = $this->dbConnection->select('node', 'n')->fields('n', ['nid'])->condition('nid', ['100003'], 'IN');;
+    $result = $query->execute();
+    $array_with_results = array_values($result->fetchAllAssoc('nid'));
+
+    return new HtmlResponse("Hola: ".json_encode($array_with_results).".");
   }
 }
