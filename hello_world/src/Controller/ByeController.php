c9a09fd3ed0204e52ced1b26a81b0bddac0740c8
francescroycampderros <francescroy@francescroy.com>
2025-03-28 17:18:27 +0100

    Exemple de com posar una plantilla des d'un controller

---
+++ hello_world/src/Controller/ByeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\hello_world\Controller\HelloController.
+ */
+namespace Drupal\hello_world\Controller;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Drupal\Core\Controller\ControllerBase;
+
+class ByeController extends ControllerBase {
+
+  public function __construct(){
+  }
+
+  public function content() {
+    return [
+    '#theme' => 'my_template',
+    '#test_var' => $this->t('Test Value'),
+    ];
+  }
+
+}
