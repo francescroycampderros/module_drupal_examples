2575367d0f7bce702dbd83be34cd0e3679e9d839
francescroycampderros <francescroy@francescroy.com>
2025-06-02 17:41:50 +0200

    Unificant modules en un sol repositori.

---
+++ rsvplist/readme.md
@@ -0,0 +1,13 @@
+ENTENC QUE AQUI NO USAREM WEBFORM...
+
+Tenim un tdc 'Event' i posarem un bloc que permet agafar el mail del usuari interessat en l'event.
+
+Farem també un lloc on veurem la llista de cada node 'event' quins interessats té.
+
+També farem una pàgina d'adaministració pel mòdul, per exemple per dir si vols que aquest event tingui el bloc de recollida o no...
+
+Usarem Forms API, tot i que crec que es podria inclus aconseguir sense la api!
+
+Com fer una manera perque sigui més fàcil de desembolupar? Vull dir que el IDE t'ajudi...
+Necessito tot el codi... Potser simplement baixar-me'l i posar a lloc el mòdul?
+wget https://ftp.drupal.org/files/projects/drupal-10.4.3.zip # descarreguem el codi, es una release! no de github!
---
+++ rsvplist/src/Form/RSVPForm.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * A form to collect an email address for RSVP details.
+ */
+
+ namespace Drupal\rsvplist\Form;
+
+ use Drupal\Core\Form\FormBase;
+ use Drupal\Core\Form\FormStateInterface;
+ use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+ class RSVPForm extends FormBase {
+
+   /**
+   * {@inheritdoc}
+   */
+    public function getFormId()
+    {
+        return 'rsvplist_email_form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $node = \Drupal::routeMatch()->getParameter('node');
+        $nid = $node->id();
+
+        $form['email'] = [
+            '#type' => 'textfield',
+            '#title' => StringTranslationTrait::t('Email address'),
+            '#size' => 25,
+            '#description' => StringTranslationTrait::t('We will send you updates to the email you provide.') ,
+            '#required' => TRUE,
+        ];
+        $form['submit'] = [
+            '#type' => 'submit',
+            '#value' => StringTranslationTrait::t('RSVP'),
+        ];
+        $form['nid'] = [
+            '#type' => 'hidden',
+            '#value' => $nid,
+        ];
+
+        return $form; 
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        
+    }
+    
+
+
+    
+ }
